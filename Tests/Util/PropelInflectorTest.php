2670534466e865c01f27beebabc79ff372e881ce
Gautrot <alexandre.gautrot@etik.com>
2026-09-10 16:37:09 +0200

    Fixed DataProvider

--- Tests/Util/PropelInflectorTest.php
+++ Tests/Util/PropelInflectorTest.php
@@ -10,6 +10,7 @@
 
 namespace Propel\Bundle\PropelBundle\Tests\Util;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use Propel\Bundle\PropelBundle\Tests\TestCase;
 use Propel\Bundle\PropelBundle\Util\PropelInflector;
 
